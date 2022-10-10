5b1f7f6157b00e75b5ba91f5fdbd85e4518bc9bf
Mehedi Hasan <mehedi.hasan.app.dev@gmail.com>
2022-10-10 21:39:28 +0600

    initial commit

---
+++ app/src/main/java/com/es/k_note_api/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.es.k_note_api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //https://notes-api-sample.herokuapp.com/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
